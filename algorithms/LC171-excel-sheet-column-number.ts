499e86cdd01b9a99401ab4032d662794b85b4977
Pradeep Kumar <kumarpradeep1010@gmail.com>
2019-09-05 14:31:55 -0500

    [LC171] Excel Sheet Column Number

---
+++ algorithms/LC171-excel-sheet-column-number.ts
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+  let length = s.length;
+  let output = 0;
+
+  if(!length) {
+    return output;
+  }
+
+  let hash = {
+    A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7, H: 8, I: 9, J: 10,
+    K: 11, L: 12, M: 13, N: 14, O: 15, P: 16, Q: 17, R: 18, S: 19,
+    T: 20, U: 21, V: 22, W: 23, X: 24, Y: 25, Z: 26
+  };
+
+  for (let char of s) {
+    output += Math.pow(26, --length) * hash[char];
+  }
+  return output;
+};
+
+titleToNumber("AB");
+titleToNumber("ZY");
+titleToNumber("AAA");
