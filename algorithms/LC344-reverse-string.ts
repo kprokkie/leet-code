4173c66dafab1e0be1df769cffee0d50528d6dbd
Pradeep Kumar <kumarpradeep1010@gmail.com>
2019-09-04 23:59:26 -0500

    [LC344] reverse string

---
+++ algorithms/LC344-reverse-string.ts
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse();
+};
+
+/**
+ * Runtime: 104 ms, faster than 94.31% of JavaScript online submissions for Reverse String.
+ * Memory Usage: 46.7 MB, less than 86.67% of JavaScript online submissions for Reverse String.
+ */
